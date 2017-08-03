9e273aefedf3fe7fc360577114416efc7d7de03c
Yuji Ueki <unhappychoice@gmail.com>
2017-08-03 14:22:29 +0900

    :bug: Fix regexp to recognize git repository

--- lib/circler/command/base_command.rb
+++ lib/circler/command/base_command.rb
@@ -21,7 +21,7 @@ module Circler
       def reponame
         repository = Rugged::Repository.new('.')
         origin = repository.remotes.find { |r| r.name == 'origin' }
-        regexp = %r{git@github.com:([\w_-]+/[\w_-]+)\.git}
+        regexp = %r{git@github.com(?::|/)([\w_-]+/[\w_-]+)(?:\.git)*}
         return Regexp.last_match(1) if origin.url =~ regexp
         nil
       end
