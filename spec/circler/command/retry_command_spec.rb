1e2b9873ee6b50121a2385c66b8fe1f3019b7a8b
Yuji Ueki <unhappychoice@gmail.com>
2019-09-22 22:28:58 +0900

    :green_heart: Add specs for RetryCommand

---
+++ spec/circler/command/retry_command_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Circler::RetryCommand, type: :command do # rubocop:disable Metrics/BlockLength
+  shared_examples_for 'a command retries build' do
+    it 'should retry build' do
+      allow(Circler::RetryCommand).to receive(:say) {}
+      expect(Circler::RetryCommand).to receive(:say).with(expected_output.strip)
+      Circler::RetryCommand.run(options)
+    end
+  end
+
+  context 'with no input' do
+    let(:options) { OpenStruct.new(project: nil, build: nil) }
+    let(:expected_url) { 'https://circleci.com/gh/unhappychoice/default_project_name_from_io' }
+    let(:expected_output) { 'build unhappychoice/default_project_name_from_io 1234 is triggered' }
+
+    it_behaves_like 'a command asks project name'
+    it_behaves_like 'a command retries build'
+  end
+
+  context 'with project input' do
+    let(:project_name) { 'unhappychoice/Circler' }
+    let(:options) { OpenStruct.new(project: project_name, build: nil) }
+    let(:expected_output) { 'build unhappychoice/Circler 1234 is triggered' }
+
+    it_behaves_like 'a command retries build'
+  end
+
+  context 'with branch input' do
+    let(:project_name) { 'unhappychoice/Circler' }
+    let(:options) { OpenStruct.new(project: nil, build: 1234) }
+    let(:expected_url) { "https://circleci.com/gh/#{project_name}" }
+    let(:expected_output) { 'build unhappychoice/Circler 1234 is triggered' }
+
+    it_behaves_like 'a command asks project name'
+    it_behaves_like 'a command retries build'
+  end
+end
